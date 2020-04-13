9d007ae38a933a6b1d1135ac8e83640895dddbdb
True Saviour <46008306+truesaviourofficial@users.noreply.github.com>
2020-04-14 00:04:51 +0530

    added build

---
+++ build/precache-manifest.8dd390ce2aa41feb91d6d0667624d56c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347cbbfb23a78119b9149a203cb10f45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2ecb0ce56310218b329",
+    "url": "/static/css/main.81a7aa86.chunk.css"
+  },
+  {
+    "revision": "ef09f71d370f4e15b7b9",
+    "url": "/static/js/2.3fce5404.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3fce5404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ecb0ce56310218b329",
+    "url": "/static/js/main.d99d7542.chunk.js"
+  },
+  {
+    "revision": "a6a4703d851e8354d005",
+    "url": "/static/js/runtime-main.09089421.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
